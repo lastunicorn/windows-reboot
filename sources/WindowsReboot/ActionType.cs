685ba745ad379f56116f10b9ab284783648be892
Iuga Alexandru <coldblackstar@yahoo.com>
2012-01-14 20:34:03 +0000

    i updated the copyright information in every file.

--- sources/WindowsReboot/ActionType.cs
+++ sources/WindowsReboot/ActionType.cs
@@ -1,5 +1,5 @@
 ﻿// Windows Reboot
-// Copyright (C) 2009 Iuga Alexandru
+// Copyright (C) 2009-2012 Dust in the Wind
 // 
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
