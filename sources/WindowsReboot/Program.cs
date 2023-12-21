d58208dc095133faa9cc1edc6663c1efb1ca5af6
Alexandru Iuga <alex.iuga@gmail.com>
2023-12-21 17:31:49 +0200

    extracted an application class

--- sources/WindowsReboot/Program.cs
+++ sources/WindowsReboot/Program.cs
@@ -15,74 +15,18 @@
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 using System;
-using System.Windows.Forms;
-using DustInTheWind.WindowsReboot.ConfigAccess;
 using DustInTheWind.WindowsReboot.Core.Config;
 using DustInTheWind.WindowsReboot.Ports.SystemAccess;
-using DustInTheWind.WindowsReboot.Presentation;
-using DustInTheWind.WindowsReboot.Presentation.MainWindow;
-using DustInTheWind.WindowsReboot.Presentation.WorkerModel;
-using DustInTheWind.WindowsReboot.Setup;
-using DustInTheWind.WindowsReboot.UserAccess;
-using WindowsReboot.SystemAccess;
-using Action = DustInTheWind.WindowsReboot.Core.Action;
-using Timer = DustInTheWind.WindowsReboot.Core.Timer;
 
 namespace DustInTheWind.WindowsReboot
 {
     internal static class Program
     {
-        private static UiDispatcher uiDispatcher;
-        private static UserInterface userInterface;
-        private static WindowsRebootConfiguration windowsRebootConfiguration;
-        private static IRebootUtil rebootUtil;
-        private static Timer timer;
-        private static Action action;
-        private static Workers workers;
-        private static ApplicationEnvironment applicationEnvironment;
-        private static MainWindowCloseBehaviour mainWindowCloseBehaviour;
-        private static MainWindowStateBehaviour mainWindowStateBehaviour;
-        private static TrayIcon trayIcon;
-
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
         [STAThread]
         private static void Main()
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-
-            WindowsRebootForm mainWindow = new WindowsRebootForm();
-            uiDispatcher = new UiDispatcher();
-            windowsRebootConfiguration = new WindowsRebootConfiguration();
-
-            userInterface = new UserInterface(uiDispatcher, windowsRebootConfiguration)
-            {
-                MainForm = mainWindow
-            };
-
-            rebootUtil = new RebootUtil();
-            timer = new Timer();
-            action = new Action(timer, rebootUtil);
-
-            IWorkerProvider workerProvider = new WorkerProvider(userInterface, timer, action);
-            workers = new Workers(workerProvider);
-
-            applicationEnvironment = new ApplicationEnvironment(action, timer, workers, windowsRebootConfiguration);
-            applicationEnvironment.Initialize();
-
-            mainWindowCloseBehaviour = new MainWindowCloseBehaviour(mainWindow, applicationEnvironment, windowsRebootConfiguration, timer, userInterface);
-            mainWindowStateBehaviour = new MainWindowStateBehaviour(mainWindow, userInterface, windowsRebootConfiguration);
-
-            mainWindow.ViewModel = new WindowsRebootViewModel(userInterface, action, timer, windowsRebootConfiguration, applicationEnvironment);
-
-            trayIcon = new TrayIcon
-            {
-                ViewModel = new TrayIconViewModel(userInterface, rebootUtil, timer, applicationEnvironment)
-            };
-
-            Application.Run(mainWindow);
+            WindowsRebootApplication application = new WindowsRebootApplication();
+            application.Run();
         }
     }
 }
---
+++ sources/WindowsReboot/WindowsRebootApplication.cs
@@ -0,0 +1,62 @@
+﻿using System.Windows.Forms;
+using DustInTheWind.WindowsReboot.ConfigAccess;
+using DustInTheWind.WindowsReboot.Core;
+using DustInTheWind.WindowsReboot.Presentation;
+using DustInTheWind.WindowsReboot.Presentation.MainWindow;
+using DustInTheWind.WindowsReboot.Presentation.WorkerModel;
+using DustInTheWind.WindowsReboot.Setup;
+using DustInTheWind.WindowsReboot.UserAccess;
+using WindowsReboot.SystemAccess;
+using Timer = DustInTheWind.WindowsReboot.Core.Timer;
+
+namespace DustInTheWind.WindowsReboot
+{
+    internal class WindowsRebootApplication
+    {
+        private readonly MainWindowCloseBehaviour mainWindowCloseBehaviour;
+        private readonly MainWindowStateBehaviour mainWindowStateBehaviour;
+        private readonly TrayIcon trayIcon;
+
+        private readonly WindowsRebootForm mainWindow;
+
+        public WindowsRebootApplication()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            mainWindow = new WindowsRebootForm();
+            UiDispatcher uiDispatcher = new UiDispatcher();
+            WindowsRebootConfiguration windowsRebootConfiguration = new WindowsRebootConfiguration();
+
+            UserInterface userInterface = new UserInterface(uiDispatcher, windowsRebootConfiguration)
+            {
+                MainForm = mainWindow
+            };
+
+            RebootUtil rebootUtil = new RebootUtil();
+            Timer timer = new Timer();
+            Action action = new Action(timer, rebootUtil);
+
+            WorkerProvider workerProvider = new WorkerProvider(userInterface, timer, action);
+            Workers workers = new Workers(workerProvider);
+
+            ApplicationEnvironment applicationEnvironment = new ApplicationEnvironment(action, timer, workers, windowsRebootConfiguration);
+            applicationEnvironment.Initialize();
+
+            mainWindowCloseBehaviour = new MainWindowCloseBehaviour(mainWindow, applicationEnvironment, windowsRebootConfiguration, timer, userInterface);
+            mainWindowStateBehaviour = new MainWindowStateBehaviour(mainWindow, userInterface, windowsRebootConfiguration);
+
+            mainWindow.ViewModel = new WindowsRebootViewModel(userInterface, action, timer, windowsRebootConfiguration, applicationEnvironment);
+
+            trayIcon = new TrayIcon
+            {
+                ViewModel = new TrayIconViewModel(userInterface, rebootUtil, timer, applicationEnvironment)
+            };
+        }
+
+        public void Run()
+        {
+            Application.Run(mainWindow);
+        }
+    }
+}
